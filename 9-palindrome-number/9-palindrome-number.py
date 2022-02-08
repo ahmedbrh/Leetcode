6bf64db3792beac33f383664f50b78d188573d62
Ahmed brhili <71940376+ahmedbrh@users.noreply.github.com>
2022-02-08 02:55:35 +0100

    Time: 68 ms (60.65%), Space: 13.3 MB (88.77%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,8 +1,18 @@
 class Solution(object):
   
-  
+    def reverse_int(self,n):
+        reverse_number =0 
+        while n>0 : 
+                reminder  = n%10  
+                reverse_number = (reverse_number*10) + reminder
+                n/=10 
+        return reverse_number
+
     def isPalindrome(self,x):
-       return False if x < 0 else x == int(str(x)[::-1])
+        if self.reverse_int(x)==x and x>=0:
+               return True  
+        else:
+                return False
 
 
         
